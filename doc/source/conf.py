5e2289ea8be45d4964c149cabf019d301029b68c
Chongdan Pan <panchongdan@foxmail.com>
2025-02-26 22:46:06 -0500

    format code

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,24 +11,30 @@ from __future__ import annotations
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
-project = 'pytemplate'
-copyright = '2024, Panda Pan'
-author = 'Panda Pan'
-release = '0.0.1'
+project = "pytemplate"
+copyright = "2024, Panda Pan"
+author = "Panda Pan"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.duration', 'sphinx.ext.napoleon', 'breathe', 'sphinx.ext.doctest']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+    "sphinx.ext.napoleon",
+    "breathe",
+    "sphinx.ext.doctest",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'piccolo_theme'
-html_static_path = ['_static']
+html_theme = "piccolo_theme"
+html_static_path = ["_static"]
--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from __future__ import annotations
 from   setuptools               import find_packages, setup
 
 setup(
-    name='pytemplate',
-    version='0.0.1',
-    description='Python Template',
-    author='Panda Pan',
-    author_email='panchongdan@gmail.com',
+    name="pytemplate",
+    version="0.0.1",
+    description="Python Template",
+    author="Panda Pan",
+    author_email="panchongdan@gmail.com",
     packages=find_packages(),
-    install_requires=['pandas>=1.4.0'],  # some install requires example
-    tests_require=['pytest>=3.3.1', 'pytest-cov>=2.5.1', 'pytest-sugar>=1.0.0'],
-    python_requires='>=3.8',
+    install_requires=["pandas>=1.4.0"],  # some install requires example
+    tests_require=[
+        "pytest>=3.3.1",
+        "pytest-cov>=2.5.1",
+        "pytest-sugar>=1.0.0",
+    ],
+    python_requires=">=3.8",
 )
